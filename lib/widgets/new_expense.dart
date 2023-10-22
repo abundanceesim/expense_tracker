5f5c9bf23f38331fce48b108e36202a14a71e02c
Abundance Esim <abundanceesim@gmail.com>
2023-10-21 23:57:39 -0400

    Add date picker functionality to New Expense modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -11,11 +12,20 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  void _presentDatePicker(){
+  void _presentDatePicker() async{
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month - 1, now.day);
-    showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: now);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+        
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   @override
@@ -50,14 +60,15 @@ class _NewExpenseState extends State<NewExpense> {
               width: 16,
             ),
             Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text('Selected Date'),
-                  IconButton(
-                      onPressed: _presentDatePicker, icon: const Icon(Icons.calendar_month))
-                ],
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(_selectedDate == null ? 'No date selected': formatter.format(_selectedDate!)),
+                IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(Icons.calendar_month))
+              ],
             ))
           ],
         ),
