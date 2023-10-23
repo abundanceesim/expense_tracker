4ab16a23e21209ffeb449ab929ffe22106b31614
Abundance Esim <abundanceesim@gmail.com>
2023-10-23 18:10:21 -0400

    Add bar chart to app

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -37,3 +37,26 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+/**
+ * Tracks the total number of expenses per category
+ */
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
