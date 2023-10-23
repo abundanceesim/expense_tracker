9729e62c530ee1deaada4eddf96806a635e37370
Abundance Esim <abundanceesim@gmail.com>
2023-10-22 23:24:30 -0400

    Remove expense items from list on left swipe

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -58,6 +58,12 @@ class _ExpenseState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense){
+    setState((){
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +73,7 @@ class _ExpenseState extends State<Expenses> {
       ]),
       body: Column(children: [
         const Text('Chart'),
-        Expanded(child: ExpensesList(expenses: _registeredExpenses))
+        Expanded(child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,))
       ]),
     );
   }
